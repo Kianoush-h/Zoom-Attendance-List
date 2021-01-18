4e6a0b3f0e5ed4f18ab157cd853548ab00fc1acd
Kianoush Haratiannejadi <haratiank2@gmail.com>
2021-01-18 10:45:28 -0500

    Initial

---
+++ ZoomAttendanceList.py
@@ -0,0 +1,64 @@
+
+"""
+@author: Kianoush 
+
+GitHUb: https://github.com/Kianoush-h
+YouTube: https://www.youtube.com/channel/UCvf9_53f6n3YjNEA4NxAkJA?view_as=subscriber
+LinkedIn: https://www.linkedin.com/in/kianoush-haratiannejadi/
+
+Email: haratiank2@gmail.com
+
+"""
+
+
+
+
+LogInUrl = 'https://us04web.zoom.us/web/sso/login?en=signin'
+
+
+
+
+
+Company_Domain = 'concordia-ca'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
